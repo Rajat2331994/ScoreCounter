76df85b3b8f087e7fdf7a6c00ffeae7e567199f6
rajat2331994 <rajatgupta2331994>
2018-02-16 00:22:11 +0530

    first commit

---
+++ app/src/main/java/com/rajat/score_counter/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.rajat.score_counter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rajat on 15/2/18.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
+
